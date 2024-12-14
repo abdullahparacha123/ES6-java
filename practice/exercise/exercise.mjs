a42d03ae7223ad0f470c6342edf592f7473f68e1
abdullahparacha123 <abdullahparacha30@gmail.com>
2024-12-14 07:49:43 -0800

    feat: exercise complete

---
+++ practice/exercise/exercise.mjs
@@ -0,0 +1,28 @@
+const alertfunction = () => {
+    const name = prompt("What is your name?", "Name");
+    {
+        document.write(`My name is ${name}.<br>`);
+    }
+    const age = prompt("What is your age?", "age");
+    {
+        document.write(`I am ${age} years old .<br>`);
+    }
+    const colification = prompt("what is your colification?", "colification")
+    {
+        document.write(`My colification is ${colification}.<br>`)
+    }
+    const email = prompt("what is your email?", "email")
+    {
+        document.write(`My Email is ${email}.<br>`)
+    }
+    const contactnumber = prompt("what is your contact number?", "contact number")
+    {
+        document.write(`My contact number is ${contactnumber}.<br>`)
+    }
+    const adreess = prompt("what is your adreess?", "adreess")
+    {
+        document.write(`My adreess is ${adreess}.<br>`)
+    }
+   
+
+}
