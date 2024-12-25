dc98b3ad55134eef0a041bf6775e53b641078a25
abdullahparacha123 <abdullahparacha30@gmail.com>
2024-12-25 08:09:07 -0800

    feat: chapter 15 done

---
+++ practice/chapter 15/chapter 15.mjs
@@ -0,0 +1,7 @@
+let marksOfClass12 =  [91,82,58,77]
+console.log(marksOfClass12 [0])
+console.log(marksOfClass12 [1])
+console.log(marksOfClass12 [2])
+console.log(marksOfClass12 [3])
+console.log(marksOfClass12 [4])
+document.write(marksOfClass12);
