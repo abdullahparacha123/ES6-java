78e3bbb70e814a7fa1e2a9a9bfac01dbf70e1b57
abdullahparacha123 <abdullahparacha30@gmail.com>
2024-12-25 09:05:06 -0800

    feat: chapter 16 done

---
+++ practice/chapter 16/chapter 16.mjs
@@ -0,0 +1,24 @@
+// let pets = []
+// pets[0]= "dog";
+// pets[1]= "cat";
+// pets[2]= "birds";
+// document.write(pets);
+// console.log(pets);
+
+
+// let pets = []
+// pets[0]= "dog";
+// pets[1]= "cat";
+// pets[2]= "bird";
+// pets.pop();
+// document.write(pets);
+// console.log(pets);
+
+
+let pets = [];
+pets[0]= "dog";
+pets[1]= "cat";
+pets[3]= "bird";
+pets.push("fish","ant");
+document.write(pets);
+console.log(pets);
