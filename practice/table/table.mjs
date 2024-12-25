a403bd15e57d2176331d6b8c6d3732a80ff9373d
abdullahparacha123 <abdullahparacha30@gmail.com>
2024-12-25 10:59:26 -0800

    feat: table done

---
+++ practice/table/table.mjs
@@ -0,0 +1,19 @@
+const alertfunction = () => {
+    const number = prompt("select any number?");
+    if (number) {
+        document.write(`multiplication of the table ${number}:<br> `);
+        document.write(`${number} x 1 = ${number * 1}<br>`);
+        document.write(`${number} x 2 = ${number * 2}<br>`);
+        document.write(`${number} x 3 = ${number * 3}<br>`);
+        document.write(`${number} x 4 = ${number * 4}<br>`);
+        document.write(`${number} x 5 = ${number * 5}<br>`);
+        document.write(`${number} x 6 = ${number * 6}<br>`);
+        document.write(`${number} x 7 = ${number * 7}<br>`);
+        document.write(`${number} x 8 = ${number * 8}<br>`);
+        document.write(`${number} x 9 = ${number * 9}<br>`);
+        document.write(`${number} x 10 = ${number * 10}<br>`);
+    }
+    else{
+        document.write(`you did'nt enter any number..`)
+    }
+}
